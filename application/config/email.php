f681a67d29ab902c79a3a627524711b015fccf61
hamzahjaya <hamzahjayaputra@gmail.com>
2020-04-08 15:20:47 +0700

    psdd

--- application/config/email.php
+++ application/config/email.php
@@ -6,7 +6,7 @@
         $config['smtp_host'] = 'ssl://smtp.googlemail.com'; //smtp host name
         $config['smtp_port'] = '465'; //smtp port number
         $config['smtp_user'] = 'hamzahjayaputrah@Gmail.com';
-        $config['smtp_pass'] = 'Erliza12'; //$from_email password
+        $config['smtp_pass'] = '@Hamzha97'; //$from_email password
         $config['mailtype'] = 'html';
         $config['charset'] = 'iso-8859-1';
         $config['wordwrap'] = TRUE;
